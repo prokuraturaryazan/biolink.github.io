4922ce4a31bb3e75189d625529c36f56fbd6bfb1
prokuraturaryazan <bd284232@gmail.com>
2026-07-28 22:46:14 +0300

    Initialize Firebase Analytics in firebase.js
    Added Firebase Analytics initialization to the config.

--- scripts/config/firebase.js
+++ scripts/config/firebase.js
@@ -1,8 +1,12 @@
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// ВСТАВЬ СВОЙ КОНФИГ СЮДА
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDnoZa72N-WLg_YWM88GmlpBcsxtA6aW5k",
   authDomain: "randevu-b6b2e.firebaseapp.com",
@@ -13,6 +17,6 @@ const firebaseConfig = {
   measurementId: "G-KELK8XNVCS"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
